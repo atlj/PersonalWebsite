e628cda2c6c97b7be45e79c9dd1b856aa62b11ff
atlj <cccswagboyyolo@yahoo.com>
2021-04-29 18:11:57 +0300

    refactor: move head to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import { AppProps } from "next/app";
 import { StyledThemeProvider } from "@definitions/styled-components";
 import { appWithTranslation } from "@i18n";
@@ -6,6 +7,25 @@ import { appWithTranslation } from "@i18n";
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <StyledThemeProvider>
+            <Head>
+                <title>Burak Güner</title>
+                <meta property="og:title" content="Burak Güner" key="title" />
+                <meta
+                    property="og:description"
+                    content="Coder, Designer, 3D Modeller."
+                    key="description"
+                />
+                <meta
+                    property="og:image"
+                    content="https://burakguner.vercel.app/res/profilePicture.png"
+                    key="image"
+                />
+                <meta
+                    property="url"
+                    content="burakguner.vercel.app"
+                    key="url"
+                />
+            </Head>
             <Component {...pageProps} />
         </StyledThemeProvider>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Head from "next/head";
 
 import { Wrapper, ContactBox } from "@components";
 import GlobalStyle from "@styles/globalStyles";
@@ -11,25 +10,7 @@ const Home: React.FC = () => {
     ] = React.useState("default");
     return (
         <Wrapper style={{ justifyContent: "center", alignItems: "center" }}>
-            <Head>
-                <title>Burak Güner</title>
-                <meta property="og:title" content="Burak Güner" key="title" />
-                <meta
-                    property="og:description"
-                    content="Coder, Designer, 3D Modeller."
-                    key="description"
-                />
-                <meta
-                    property="og:image"
-                    content="https://burakguner.vercel.app/res/profilePicture.png"
-                    key="image"
-                />
-                <meta
-                    property="url"
-                    content="burakguner.vercel.app"
-                    key="url"
-                />
-            </Head>
+          
             <GlobalStyle />
             <ContactBox
                 onClick={() => {
