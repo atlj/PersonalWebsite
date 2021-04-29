52b653ee3f19798f377f68bc893017bf61c6ea13
atlj <cccswagboyyolo@yahoo.com>
2021-04-29 17:38:15 +0300

    feat: add metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 
 import { Wrapper, ContactBox } from "@components";
 import GlobalStyle from "@styles/globalStyles";
@@ -10,6 +11,25 @@ const Home: React.FC = () => {
     ] = React.useState("default");
     return (
         <Wrapper style={{ justifyContent: "center", alignItems: "center" }}>
+            <Head>
+                <title>Burak Güner</title>
+                <meta property="og:title" content="Burak Güner" key="title" />
+                <meta
+                    property="og:description"
+                    content="Coder, Designer, 3D Modeller."
+                    key="description"
+                />
+                <meta
+                    property="og:image"
+                    content="https://burakguner.vercel.app/res/profilePicture.png"
+                    key="image"
+                />
+                <meta
+                    property="url"
+                    content="burakguner.vercel.app"
+                    key="url"
+                />
+            </Head>
             <GlobalStyle />
             <ContactBox
                 onClick={() => {
