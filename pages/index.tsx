cb202341b3b6b59108cfab07683554c433e216a5
atlj <cccswagboyyolo@yahoo.com>
2021-04-29 12:39:47 +0300

    refactor: change main index for testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 
-import { Wrapper, Header, Main, Footer, Cards } from "@components";
+import { Wrapper, ContactBox } from "@components";
 import GlobalStyle from "@styles/globalStyles";
 
 const Home: React.FC = () => {
+    const [variant, useVariant]: [
+        variant: "default" | "contact",
+        useVariant: React.Dispatch<React.SetStateAction<"default" | "contact">>,
+    ] = React.useState("default");
     return (
-        <Wrapper>
+        <Wrapper style={{ justifyContent: "center", alignItems: "center" }}>
             <GlobalStyle />
-            <Header />
-            <Main />
-            <Cards />
-            <Footer />
+            <ContactBox
+                onClick={() => {
+                    useVariant(variant === "default" ? "contact" : "default");
+                }}
+                variant={variant}
+            />
         </Wrapper>
     );
 };
