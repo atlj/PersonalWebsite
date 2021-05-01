dcf2c049a01a36bf73bc3d10296432874cc75fca
atlj <cccswagboyyolo@yahoo.com>
2021-05-01 16:17:21 +0300

    feat: add gradient colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
     purge: ["./pages/**/*.tsx", "./src/**/*.tsx"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                ProgrammingFrom: "#5182FF",
+                ProgrammingTo: "#F4ACE4",
+                GraphicDesignFrom: "#A276FF",
+                GraphicDesignTo: "#59A9F2",
+                ModelingFrom: "#F7C59F",
+                ModelingTo: "#F285C7",
+                
+            },
+        },
     },
     variants: {
         extend: {},
