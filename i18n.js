da1fb8eb3648af90195495093a83dca4c259d859
atlj <cccswagboyyolo@yahoo.com>
2021-06-25 18:51:03 +0300

    refactor: eslint fixes

--- i18n.js
+++ i18n.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const NextI18Next = require("next-i18next").default;
 const { localeSubpaths } = require("next/config").default().publicRuntimeConfig;
 const path = require("path");
